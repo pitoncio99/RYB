0a8eeb05df231abf8502be9063419bed3469ed54
Victor Marquez <victor.paez.1clsf@gmail.com>
2025-02-18 18:19:31 -0300

    AVANCE 1

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById('btn-action');
-    if (btn) {
-        btn.addEventListener('click', function() {
-            alert('¡Gracias por contactarnos! Nos pondremos en contacto contigo pronto.');
-        });
-    }
-});
